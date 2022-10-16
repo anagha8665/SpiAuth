8edb27f2be3f4a70165a483efd36f33dfbc15885
anagha8665 <anaghakj2001@gmail.com>
2022-10-16 09:22:23 +0530

    second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginform!:FormGroup
 
-  constructor() { }
+  constructor(private formbuilder:FormBuilder,private http:HttpClient,private r:Router) { }
 
   ngOnInit(): void {
+    this.loginform=this.formbuilder.group(
+      {
+        Email:[''],Password:['']
+      }
+    )
+  }
+  login(){
+    this.http.get<any>('http://localhost:3000/users').subscribe(res=>{const user=res.find((a:any)=>{
+      return a.Email===this.loginform.value.Email &&
+      a.Password===this.loginform.value.Password
+
+    })
+    if(user){
+      alert("User exist");
+      this.loginform.reset;
+    }
+    else{
+      alert("no user");
+    }
+  })
+
   }
 
 }
